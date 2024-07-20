b3d369666a3fb0500ab92c5fd161da39ea771049
devsandanayake <devinsandanayake49@gmail.com>
2024-07-20 14:34:59 +0530

    update

--- backend/src/routes/ads.js
+++ backend/src/routes/ads.js
@@ -1,15 +1,20 @@
-const {createAd,upload , addWatermark,viewAllAds,approved,viewSpecificAd} = require('../controllers/ads');
+const {createAd,upload , addWatermark,viewAllAds,approved,viewSpecificAd,viewAllAdsForAdmin} = require('../controllers/ads');
 const express = require('express');
 const router = express.Router();
 const authMiddelware = require('../middlewares/authuser');
 
 
-router.post('/createAds', authMiddelware, upload.array('images') ,addWatermark, createAd);
+router.post('/createAds', authMiddelware.authUser, upload.array('images') ,addWatermark, createAd);
 router.get('/viewAds', viewAllAds);
 //approvel ads
 router.patch('/approveAds', approved);
 
 //view specific ad
 router.get('/viewSpecificAd/:adCode', viewSpecificAd);
+
+
+
+//admin Routes
+router.get('/viewAllAdsForAdmin',authMiddelware.authAdmin, viewAllAdsForAdmin);
 
 module.exports = router;
