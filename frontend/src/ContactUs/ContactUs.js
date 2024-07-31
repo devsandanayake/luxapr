c9eb027809969dea77481a98145c74521f175324
ChamodSGK <kalanachamodac@gmail.com>
2024-07-31 12:29:47 +0530

    uninstall pannelum

--- frontend/src/ContactUs/ContactUs.js
+++ frontend/src/ContactUs/ContactUs.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom';
-import background from '../Images/ap6.jpg';
 
 export default function ContactUs() {
 
@@ -19,20 +18,10 @@ export default function ContactUs() {
 
   return (
     <>
-         <div
-                style={{ 
-                    height: 'calc(100vh - 60px)', 
-                    backgroundImage: `url(${background})`, 
-                    backgroundSize: 'cover', 
-                    backgroundPosition: 'center',
-                }}
-            >
+         <div>
 
-                <button className="bg-blue-500 mt-10 ml-10 p-1 rounded "
-                onClick={AddApartment}>Add Apartment</button>
-
-
-            </div>
+                
+        </div>
     </>
   )
 }
