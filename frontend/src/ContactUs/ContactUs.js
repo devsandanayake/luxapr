877370c288506e3a7819272009b869b236bb2aae
ChamodSGK <kalanachamodac@gmail.com>
2024-08-01 10:41:55 +0530

    contactus page

--- frontend/src/ContactUs/ContactUs.js
+++ frontend/src/ContactUs/ContactUs.js
@@ -1,5 +1,8 @@
-import React from 'react'
+import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope, faPhone, faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons';
+import './ContactUs.css';
 
 export default function ContactUs() {
 
@@ -14,14 +17,82 @@ export default function ContactUs() {
         } else {
             navigate('/login');
         }
-    }
+    };
 
-  return (
-    <>
-         <div>
+    return (
+        <>
+            <div className="backgroundd"></div>
+           
+                <div className="ContactUs-title">Contact Us</div>
 
-                
-        </div>
-    </>
-  )
+                <div className='Apartment-add'>
+                    <div className='desc'>
+                Want to list your apartment for sale or rent?
+                </div>
+
+                <button className='add-apartment' onClick={AddApartment}>Add Apartment</button>
+                </div>
+
+
+                <div className="content">
+                    <div className="ContactUs-details">
+                        <div className='contenttitle'>
+                        If you have any questions or need further information, please feel free to contact us. Our team is here to assist you!
+                        </div>
+                        <div className="contact-info">
+                            <div className="contact-item">
+                                <FontAwesomeIcon icon={faPhone} className='fa-icon text-gold text-3xl' />
+                                <div>
+                                    
+                                    <p className=' underline'>+94 123 456 789</p>
+                                </div>
+                            </div>
+                            <div className="contact-item">
+                                <FontAwesomeIcon icon={faEnvelope} className='fa-icon text-gold text-3xl' />
+                                <div>
+                                    <p>info@luxuryapartments.lk</p>
+                                </div>
+                            </div>
+                            <div className="contact-item">
+                                <FontAwesomeIcon icon={faMapMarkerAlt} className='fa-icon text-gold text-3xl' />
+                                <div>
+                                    <p>123 Luxury St, Colombo, Sri Lanka</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+
+                {/* <div className="content">
+                <div className="ContactUs-details">
+                    <p>If you have any questions or need further information, please feel free to contact us. Our team is here to assist you!</p>
+                    <div className="contact-info">
+                        <div className="contact-item">
+                            <FontAwesomeIcon icon={faPhone} className='fa-icon text-gold text-3xl' />
+                            <div>
+                                <h3>Emergency Help</h3>
+                                <p>+94 123 456 789</p>
+                            </div>
+                        </div>
+                        <div className="contact-item">
+                            <FontAwesomeIcon icon={faEnvelope} className='fa-icon text-gold text-3xl' />
+                            <div>
+                                <h3>Quick Email</h3>
+                                <p>info@luxuryapartments.lk</p>
+                            </div>
+                        </div>
+                        <div className="contact-item">
+                            <FontAwesomeIcon icon={faMapMarkerAlt} className='fa-icon text-gold text-3xl' />
+                            <div>
+                                <h3>Office Address</h3>
+                                <p>123 Luxury St, Colombo, Sri Lanka</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div> */}
+        </>
+    );
 }
