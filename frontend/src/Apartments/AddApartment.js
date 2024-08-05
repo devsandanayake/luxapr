7ffb80b8e28e86fb07732d95f620e26e83551a59
ChamodSGK <kalanachamodac@gmail.com>
2024-08-05 13:08:31 +0530

    Add apartment page design

--- frontend/src/Apartments/AddApartment.js
+++ frontend/src/Apartments/AddApartment.js
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import axiosInstance from '../axiosConfig';
 import './AddApartment.css';
 import { useParams, useLocation } from 'react-router-dom';
+import { FaHome, FaMapMarkerAlt, FaCity, FaBuilding, FaBed, FaBath, FaLayerGroup, FaRulerCombined, FaDollarSign, FaMoneyBillWave } from 'react-icons/fa';
+
 
 export default function AddApartment() {
   const { id } = useParams();
@@ -29,7 +31,9 @@ export default function AddApartment() {
     transactionType: value,
   });
   const [images, setImages] = useState([]);
+  const [imageUrls, setImageUrls] = useState([]);
   const [images360, setImages360] = useState([]);
+  const [image360Urls, setImage360Urls] = useState([]);
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -78,13 +82,41 @@ export default function AddApartment() {
   const handleImageChange = (e) => {
     const selectedFiles = Array.from(e.target.files);
     setImages((prevImages) => [...prevImages, ...selectedFiles]);
+
+    const urls = selectedFiles.map((file) => URL.createObjectURL(file));
+    setImageUrls((prevUrls) => [...prevUrls, ...urls]);
+
     e.target.value = null; // Clear the input value to allow re-selecting the same file if needed
   };
 
   const handleImage360Change = (e) => {
     const selectedFiles = Array.from(e.target.files);
     setImages360((prevImages360) => [...prevImages360, ...selectedFiles]);
+
+    const urls = selectedFiles.map((file) => URL.createObjectURL(file));
+    setImage360Urls((prevUrls) => [...prevUrls, ...urls]);
+
     e.target.value = null; // Clear the input value to allow re-selecting the same file if needed
+  };
+
+  const removeImage = (index) => {
+    const newImages = [...images];
+    newImages.splice(index, 1);
+    setImages(newImages);
+
+    const newImageUrls = [...imageUrls];
+    newImageUrls.splice(index, 1);
+    setImageUrls(newImageUrls);
+  };
+
+  const removeImage360 = (index) => {
+    const newImages360 = [...images360];
+    newImages360.splice(index, 1);
+    setImages360(newImages360);
+
+    const newImage360Urls = [...image360Urls];
+    newImage360Urls.splice(index, 1);
+    setImage360Urls(newImage360Urls);
   };
 
   const handleSubmit = async (e) => {
@@ -139,161 +171,174 @@ export default function AddApartment() {
   return (
     <div className="full-screen">
       <div className='w-9/12'>
-        <h2 className='text-3xl mt-4 text-center' style={{ fontFamily: 'Georgia, serif' }}>Add Your Apartment For {titlebyvalue()}</h2>
-        <form className='mt-3 ml-3 w-full mb-5' onSubmit={handleSubmit}>
+        <h2 className='text-4xl mt-4 text-center' style={{ fontFamily: 'Georgia, serif' }}>Add Your Apartment For {titlebyvalue()}</h2>
+        <form className='mt-10 ml-3 w-full mb-5' onSubmit={handleSubmit}>
 
-          <div className='mt-2'>
-            <label className='text-lg' htmlFor="title">Title</label>
-            <div>
-              <input
-                type="text"
-                name="title"
-                onChange={handleChange}
-                className="input"
-                required
-              />
-            </div>
-          </div>
+        <div className='mt-2'>
+        <label className='text-lg' htmlFor="title">Title</label>
+        <div className="relative">
+          <FaHome className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gold" />
+          <input
+            type="text"
+            name="title"
+            onChange={handleChange}
+            className="input pl-10" // Add padding to the left to make space for the icon
+            required
+          />
+        </div>
+      </div>
 
+          
           <div className='flex justify-between'>
             <div className='mt-2'>
               <label className='text-lg' htmlFor="address.postCode">Address Line 1</label>
-              <div>
+              <div className="relative">
+                <FaMapMarkerAlt className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gold" />
                 <input
                   type="text"
                   name="address.postCode"
                   onChange={handleChange}
-                  className="addressinput"
+                  className="addressinput pl-10"
                   required
                 />
               </div>
             </div>
-
+          
             <div className='mt-2'>
               <label className='text-lg' htmlFor="address.street">Address Line 2</label>
-              <div>
+              <div className="relative">
+                <FaMapMarkerAlt className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gold" />
                 <input
                   type="text"
                   name="address.street"
                   onChange={handleChange}
-                  className="addressinput"
+                  className="addressinput pl-10"
                   required
                 />
               </div>
             </div>
-
+          
             <div className='mt-2'>
               <label className='text-lg' htmlFor="areas">City</label>
-              <div>
+              <div className="relative">
+                <FaCity className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gold" />
                 <input
                   type="text"
                   name="areas"
                   onChange={handleChange}
-                  className="addressinput"
+                  className="addressinput pl-10"
                   required
                 />
               </div>
             </div>
-
+          
             <div className='mt-2'>
               <label className='text-lg' htmlFor="districts">District</label>
-              <div>
+              <div className="relative">
+                <FaBuilding className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gold" />
                 <input
                   type="text"
                   name="districts"
                   onChange={handleChange}
-                  className="addressinput"
+                  className="addressinput pl-10"
                   required
                 />
               </div>
             </div>
           </div>
-
+          
           <div className='mt-2'>
             <label className='text-lg' htmlFor="description">Description</label>
             <div>
               <textarea
                 name="description"
                 onChange={handleChange}
-                className="input"
+                className="Descinput"
+                placeholder="Enter description"
                 required
               ></textarea>
             </div>
           </div>
-
+          
           <div className='mt-2'>
             <label className='text-lg' htmlFor="bedroomCount">Bedroom Count</label>
-            <div>
+            <div className="relative">
+              <FaBed className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gold" />
               <input
                 type="number"
                 name="bedroomCount"
                 onChange={handleChange}
-                className="input"
+                className="input pl-10"
                 required
               />
             </div>
           </div>
-
+          
           <div className='mt-2'>
             <label className='text-lg' htmlFor="bathroomCount">Bathroom Count</label>
-            <div>
+            <div className="relative">
+              <FaBath className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gold" />
               <input
                 type="number"
                 name="bathroomCount"
                 onChange={handleChange}
-                className="input"
+                className="input pl-10"
                 required
               />
             </div>
           </div>
-
+          
           <div className='mt-2'>
             <label className='text-lg' htmlFor="floor">Floor</label>
-            <div>
+            <div className="relative">
+              <FaLayerGroup className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gold" />
               <input
                 type="number"
                 name="floor"
                 onChange={handleChange}
-                className="input"
+                className="input pl-10"
                 required
               />
             </div>
           </div>
-
+          
           <div className='mt-2'>
             <label className='text-lg' htmlFor="areaSize">Area Size</label>
-            <div>
+            <div className="relative">
+              <FaRulerCombined className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gold" />
               <input
                 type="number"
                 name="areaSize"
                 onChange={handleChange}
-                className="input"
+                className="input pl-10"
                 required
               />
             </div>
           </div>
-
+          
           <div className='mt-2'>
-            <label className='text-lg' htmlFor="price">Price ({Currencylabel()})</label>
-            <div>
+            <label className='text-lg' htmlFor="price">Price {Currencylabel()}</label>
+            <div className="relative">
+              <FaDollarSign className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gold" />
               <input
                 type="number"
                 name="price"
                 onChange={handleChange}
-                className="input"
+                className="input pl-10"
                 required
               />
             </div>
           </div>
-
+          
           <div className='mt-2'>
-            <label className='text-lg' htmlFor="currency">Currency </label>
-            <div>
+            <label className='text-lg' htmlFor="currency">Currency</label>
+            <div className="relative">
+              <FaMoneyBillWave className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gold" />
               <input
                 type="text"
                 name="currency"
                 onChange={handleChange}
-                className="input"
+                className="input pl-10"
                 required
               />
             </div>
@@ -316,11 +361,24 @@ export default function AddApartment() {
               {images.length > 0 && (
                 <div className="mt-2">
                   {images.length} file(s) selected
-                  <ul>
-                    {images.map((file, index) => (
-                      <li key={index}>{file.name}</li>
+                  <div className="flex flex-wrap">
+                    {imageUrls.map((url, index) => (
+                      <div key={index} className="relative">
+                        <img
+                          src={url}
+                          alt={`Selected file ${index + 1}`}
+                          className="w-16 h-16 object-cover m-1"
+                        />
+                        <button
+                          type="button"
+                          className="absolute top-0 right-0 bg-red-500 text-white p-1 rounded-full"
+                          onClick={() => removeImage(index)}
+                        >
+                          &times;
+                        </button>
+                      </div>
                     ))}
-                  </ul>
+                  </div>
                 </div>
               )}
             </div>
@@ -343,11 +401,24 @@ export default function AddApartment() {
               {images360.length > 0 && (
                 <div className="mt-2">
                   {images360.length} file(s) selected
-                  <ul>
-                    {images360.map((file, index) => (
-                      <li key={index}>{file.name}</li>
+                  <div className="flex flex-wrap">
+                    {image360Urls.map((url, index) => (
+                      <div key={index} className="relative">
+                        <img
+                          src={url}
+                          alt={`Selected file ${index + 1}`}
+                          className="w-16 h-16 object-cover m-1"
+                        />
+                        <button
+                          type="button"
+                          className="absolute top-0 right-0 bg-red-500 text-white rounded-full"
+                          onClick={() => removeImage360(index)}
+                        >
+                          &times;
+                        </button>
+                      </div>
                     ))}
-                  </ul>
+                  </div>
                 </div>
               )}
             </div>
