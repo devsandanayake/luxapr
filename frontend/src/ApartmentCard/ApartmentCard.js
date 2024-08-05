57ca0995cb6633512de4e775880d794c3beacebb
ChamodSGK <kalanachamodac@gmail.com>
2024-08-05 17:03:22 +0530

    Card view

--- frontend/src/ApartmentCard/ApartmentCard.js
+++ frontend/src/ApartmentCard/ApartmentCard.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import './ApartmentCard.css'; // Import the CSS file for styling
 import background from '../Images/bg2.jpg'; 
 import { IoBedOutline} from "react-icons/io5";
@@ -8,12 +8,41 @@ import { FaShower } from "react-icons/fa";
 
 const ApartmentCard = ({ apartment }) => {
     const [isHovered, setIsHovered] = useState(false);
+    const navigate = useNavigate();
+
+    const truncatedTitle = apartment.title.length > 80 ? apartment.title.substring(0, 80) + '...' : apartment.title;
+
+    const priceTag = (() => {
+        if (apartment.transactionType === 1) {
+            return "Per Day";
+        } else if (apartment.transactionType === 2) {
+            return "Per Month";
+        } else if (apartment.transactionType === 3) {
+            return "Per Day";
+        } else {
+            return "";
+        }
+    })();
+
+    const TermTag = (() => {
+        if (apartment.transactionType === 1) {
+            return "Short-Term";
+        } else if (apartment.transactionType === 2) {
+            return "Long-Term";
+        } else if (apartment.transactionType === 3) {
+            return "Short / Long Term";
+        } else if (apartment.transactionType === 4) {
+            return "Auction";
+        }
+    })();
+
 
     return (
         <div
             className="card"
             onMouseEnter={() => setIsHovered(true)}
             onMouseLeave={() => setIsHovered(false)}
+            onClick={() => navigate(`/viewapartment?adcode=${apartment.adCode}`)}
         >
             <div className="image-container">
                 <img 
@@ -23,18 +52,18 @@ const ApartmentCard = ({ apartment }) => {
                     alt={apartment.name || "Room"} 
                     className="room-image" 
                 />
-                <div className="price-tag">LKR. &nbsp;{apartment.price}/=</div>
+                {apartment.transactionType !== 3 && (
+                <div className="price-tag">LKR. &nbsp;{apartment.price}/= {priceTag}</div>
+                )}
+                <div className="term-tag">{TermTag}</div>
             </div>
             <div className={`details ${isHovered ? 'expanded' : ''}`}>
-                <h3>{apartment.title}</h3>
-                <p>{apartment.districts}, &nbsp; {apartment.areas}</p>
+                <h3>{truncatedTitle}</h3>
+                <p>{apartment.areas}, &nbsp; {apartment.districts}</p>
                 <div className="info">
                     <span><IoBedOutline />({apartment.bedroomCount}) Bed's</span>
                     <span><FaShower />({apartment.bathroomCount}) Baths</span>
                 </div>
-                <div className="read-more">
-  <Link to={`/viewapartment?adcode=${apartment.adCode}`}>READ MORE</Link>
-</div>
             </div>
         </div>
     );
