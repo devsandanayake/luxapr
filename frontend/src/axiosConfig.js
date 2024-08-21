40535afe656124b2723868f9f3cb1871ce8d346b
ChamodSGK <kalanachamodac@gmail.com>
2024-08-21 14:51:15 +0530

    axios

--- frontend/src/axiosConfig.js
+++ frontend/src/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:8081',
+  baseURL: 'http://124.43.179.118:8081',
 });
 
 // Add an interceptor to include the token in every request
