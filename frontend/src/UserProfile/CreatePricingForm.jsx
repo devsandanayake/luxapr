1e8097e304491e24bc93baf6130075cfe3ce9094
devsandanayake <devinsandanayake49@gmail.com>
2024-08-26 09:57:00 +0530

    up

--- frontend/src/UserProfile/CreatePricingForm.jsx
+++ frontend/src/UserProfile/CreatePricingForm.jsx
@@ -72,7 +72,7 @@ const CreatePricingForm = ({ adCode: initialAdCode }) => {
     };
 
     return (
-        <>{transactionType}
+        <>
         <Form onFinish={handleSubmit} layout="vertical">
             <Form.Item label="Ad Code" required>
                 <Input
@@ -86,6 +86,7 @@ const CreatePricingForm = ({ adCode: initialAdCode }) => {
                     type="text"
                     value={transactionType}
                     onChange={(e) => setTransactionType(e.target.value)}
+                    readOnly
                     
                 />
             </Form.Item>
